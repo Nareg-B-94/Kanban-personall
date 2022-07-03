a598c175b32f437e52f145c716af9d3df2a50b66
Nareg Berberian <naregb94@gmail.com>
2022-07-03 11:06:29 +0200

    addTask functions are kinda done

--- addTask.js
+++ addTask.js
@@ -10,25 +10,34 @@ let users = [
     'email': 'salma.hayek@company-email.com',
   },
   {
-   'picture': 'a3.PNG',
-   'name': 'miss Redhead',
-   'email': 'redhead@company-email.com'
-},
-{
-  'picture': 'a4.PNG',
-  'name': 'Slavik Bratan',
-  'email': 'bratinkaSlavo@company-email.com'
+    'picture': 'a3.PNG',
+    'name': 'miss Redhead',
+    'email': 'redhead@company-email.com'
+  },
+  {
+    'picture': 'a4.PNG',
+    'name': 'Slavik Bratan',
+    'email': 'bratinkaSlavo@company-email.com'
   }
-
 ];
+
 let selectedUsers = [];
+let tasks = []; //The JSON that will be transported between the pages.
+
+let title;
+let date;
+let category;
+let urgency;
+let description;
+let createdAt;
+
 
 //Renders the Avatars
 function render() {
   document.getElementById("avatars");
   avatars.innerHTML = "";
   for (let i = 0; i < users.length; i++) {
-    const user = users[i];
+    const user = users[i]['picture'];
     avatars.innerHTML += `
         <img id="user-${i}" onclick="selectedAvatar(${i})" class="avatarPic" src="img/${user}" alt="">
         `;
@@ -49,7 +58,37 @@ function selectedAvatar(i) {
 //cancels the Task and gets back to board.html
 function cancelTask() {
   window.location.href = "board.html";
-}
+};
+
+//storing the values of the fields in the global variables
+function saveValues() {
+  title = document.getElementById('title').value;
+  date = document.getElementById('datePicker').value;
+  category = document.getElementById('category').value;
+  urgency = document.getElementById('urgency').value;
+  description = document.getElementById('description').value;
+};
 
 //Creates the Task and adds it to the board.html
-function createTheTask() {}
+function createTheTask(event) {
+  saveValues();
+  let time = new Date().getTime();
+  createdAt = new Date(time).toLocaleString(); //uploads the date in a text form
+  event.preventDefault();
+  tasks.push({
+    title, date, category, urgency, description
+  })
+  console.log(tasks);
+  clearInputFields();
+
+};
+
+
+function clearInputFields(){
+  saveValues();
+  title.value=``;
+  date.value=``;
+  category.value=``;
+  urgency.value=``;
+  description.value=``;
+}
